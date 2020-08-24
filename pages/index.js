1084c5d4a2fe0e39d414cbddc9f1861640ed7bf6
Carlos Rangel <carlos.developer17@gmail.com>
2020-08-24 16:07:32 -0500

    Removed unnecessary import

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Layout from '../components/Layout'
 import Signin from '../components/Signin'
-import ReactLogo from '../assets/logo.svg';
 
 const Index = () => (
     <Layout shownavbar={false}>
